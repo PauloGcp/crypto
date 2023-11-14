f4ca157dd33cf96c2114bc0955317d809d01ebee
Paulo Guilherme <pauloguilherme.c@hotmail.com>
2023-11-14 08:44:13 -0300

    Conteúdo ate rotas: aula 50

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  # mesma coisa de "get 'welcome/index', to: 'welcome#index'" mas pela rota ter a mesma formação de 'controler#action' podemos ocultar o 'to'
   resources :coins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # a rota padrão será apontada para o controller 'welcome' e action 'index'
+  root to: 'welcome#index'
 end
